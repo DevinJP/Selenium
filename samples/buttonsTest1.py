583426a319092f4876335e783ce7e54c62aa9a14
DevinJP <djp0596@yahoo.com>
2017-01-23 14:02:49 -0500

    Add files via upload
    ButtonsTest1 checks for all buttons on the page and then will click if the button's face value contains "Accept", "Submit", "Connect" etc.

---
+++ samples/buttonsTest1.py
@@ -0,0 +1,33 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("google.com")
+	time.sleep(5)
+	buttons = driver.find_elements_by_tag_name("button")
+	for i in buttons
+		button = buttons[i]
+		if "Accept" in button.getText()
+			button.click()
+			return
+		if "Connect" in button.getText()
+			button.click()
+			return
+		if "Submit" in button.getText()
+			button.click()
+			return	
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
